504f7f78e30cfdf6bc7b0ea55d0f97b2b58e1ad2
Roshan Chilap <roshanchilap143@gmail.com>
2026-03-15 13:19:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ const App = () => {
   };
 
   const getRecipes = async () => {
-    const response = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-    );
+     const response = await fetch(
+    `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+  );
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits);
